a60ce7b2c2571c6a7c73edeb7a6307bce4a26c75
Khashayar Shomali <khashi792017@gmail.com>
2023-04-28 20:03:39 +0330

    Create login form.

---
+++ landa.sme.login/src/layouts/MainLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container" dir="ltr">
+    <q-layout view="rHh rpR fFf" container style="height: 100vh">
+      <TheSidebar />
+      <q-page-container>
+        <q-page>
+          <router-view />
+        </q-page>
+      </q-page-container>
+    </q-layout>
+  </div>
+</template>
+
+<script setup></script>
+
+<style>
+* {
+  font-family: "Vazirmatn", sans-serif;
+}
+</style>
